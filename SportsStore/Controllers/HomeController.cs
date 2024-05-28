133faf437b94856dea43da9e3a9235cdd356a04f
NGÔ THANH THIÊN <skycorn4@gmail.com>
2024-05-28 22:43:56 +0700

    first commit

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Controllers
+{
+  public class HomeController : Controller
+  {
+    private IStoreRepository repository;
+
+
+
+    public HomeController(IStoreRepository repo)
+    {
+      repository = repo;
+    }
+    public IActionResult Index() => View(repository.Products);
+  }
+}
